39a1a38ac6d39fa702ea66cd4a98f5510feb7700
helenaleivas <lennaleivas@gmail.com>
2018-06-04 22:59:54 -0300

    Update index1.js

--- place/index1.js
+++ place/index1.js
@@ -1,6 +1,7 @@
 const enviar = document.querySelector('#form');
 const nome = document.querySelector('#nome');
 const email = document.querySelector('#email');
+const senha = document.querySelector('#password');
 const checks = document.querySelectorAll('.form-check-input');
 enviar.addEventListener('submit', function (e) {
   e.preventDefault();
@@ -12,6 +13,7 @@ const form = document.querySelector('form');
 form.addEventListener('submit', function (e) {
   localStorage.setItem("nome", nome.value);
   localStorage.setItem("email", email.value);
+  localStorage.setItem("senha", senha.value);
   form.reset();
   e.preventDefault();
 });
